7d14a6be33fc75341250848f1d127e69fba9c555
Benjamin <Benjamin@gmail.com>
2025-03-29 21:25:58 +0700

    F-003: implement delete feature

--- patient-service/src/main/java/com/benjamin/patientservice/services/PatientService.java
+++ patient-service/src/main/java/com/benjamin/patientservice/services/PatientService.java
@@ -57,5 +57,8 @@ public class PatientService implements IPatientService {
 		return PatientMapper.toDto(patient);
 	}
 
-
+	@Override
+	public void deletePatient(UUID id) {
+		patientRepository.deleteById(id);
+	}
 }
