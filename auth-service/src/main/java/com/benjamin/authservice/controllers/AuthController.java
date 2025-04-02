9b44e15ac24e726d3417ad97926221bf01af4fa3
trungbeso <trungbeso@gmail.com>
2025-04-02 15:15:13 +0700

    F-017: using jwt to build authenticate feature

---
+++ auth-service/src/main/java/com/benjamin/authservice/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package com.benjamin.authservice.controllers;
+
+import com.benjamin.authservice.dtos.LoginRequestDto;
+import com.benjamin.authservice.dtos.LoginResponseDto;
+import com.benjamin.authservice.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login (@RequestBody LoginRequestDto request) {
+
+        Optional<String> tokenOptional = authService.authenticate(request);
+        if (tokenOptional.isEmpty()) {
+            log.error("----------Not authenticated");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+}
---
+++ auth-service/src/main/java/com/benjamin/authservice/services/UserService.java
@@ -0,0 +1,13 @@
+package com.benjamin.authservice.services;
+
+import com.benjamin.authservice.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+
+}
