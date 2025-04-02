dd476f3d0cef0fe9b53854d5e6d704a22d2d1329
trungbeso <trungbeso@gmail.com>
2025-04-02 15:59:55 +0700

    F-017: validate jwt access token

--- auth-service/src/main/java/com/benjamin/authservice/services/AuthService.java
+++ auth-service/src/main/java/com/benjamin/authservice/services/AuthService.java
@@ -1,9 +1,11 @@
 package com.benjamin.authservice.services;
 
 import com.benjamin.authservice.dtos.LoginRequestDto;
+import com.benjamin.authservice.dtos.LoginResponseDto;
 import com.benjamin.authservice.models.Uzer;
 import com.benjamin.authservice.repositories.UserRepository;
 import com.benjamin.authservice.utils.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,18 +13,12 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
-
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder,
-                       JwtUtil jwtUtil) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtil = jwtUtil;
-    }
 
     public Optional<String> authenticate (LoginRequestDto request) {
         Optional<String> token = userRepository
@@ -30,5 +26,14 @@ public class AuthService {
                 .filter(u -> passwordEncoder.matches(request.getPassword(), u.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
         return token;
+    }
+
+    public boolean validateToken (String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
--- auth-service/src/main/java/com/benjamin/authservice/utils/JwtUtil.java
+++ auth-service/src/main/java/com/benjamin/authservice/utils/JwtUtil.java
@@ -1,15 +1,20 @@
 package com.benjamin.authservice.utils;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JwtUtil {
     private final Key secretKey;
@@ -28,5 +33,19 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 5)) /* 5 hours */
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts
+                    .parser()
+                    .verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature");
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT token");
+        }
     }
 }
