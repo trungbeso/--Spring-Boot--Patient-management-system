7c10936967aa67f72322823d15e2cb34e5813c7d
trungbeso <trungbeso@gmail.com>
2025-04-02 13:46:02 +0700

    F-015:create docker file for auth service, run and deploy with data seed

---
+++ auth-service/src/main/java/com/benjamin/authservice/models/Uzer.java
@@ -0,0 +1,57 @@
+package com.benjamin.authservice.models;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class Uzer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+//    1 user have only 1 role
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
